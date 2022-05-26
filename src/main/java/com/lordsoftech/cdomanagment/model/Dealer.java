166135178ae34f04da84d508bf1c79bb79a6b8e6
tylnesh <kohutek.michal@gmail.com>
2022-05-26 21:28:33 +0200

    Cleaned up and started on creating a new parent class for controllers

---
+++ src/main/java/com/lordsoftech/cdomanagment/model/Dealer.java
@@ -0,0 +1,32 @@
+package com.lordsoftech.cdomanagment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "dealers")
+@Getter
+@Setter
+public class Dealer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dealer_generator")
+    @SequenceGenerator(name = "dealer_generator", sequenceName = "dealer_seq", allocationSize = 1)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(columnDefinition = "varchar(255) default ''")
+    private String dealer;
+
+    @Column(columnDefinition = "varchar(255) default ''")
+    private String slug;
+
+    public Dealer() {
+    }
+}
