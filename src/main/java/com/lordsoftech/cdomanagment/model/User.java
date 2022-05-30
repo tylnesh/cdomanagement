ad9c57307e75c436c452ccd901b60f506da762c9
tylnesh <kohutek.michal@gmail.com>
2022-05-30 11:26:48 +0200

    Added user class

---
+++ src/main/java/com/lordsoftech/cdomanagment/model/User.java
@@ -0,0 +1,41 @@
+package com.lordsoftech.cdomanagment.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
+    @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(columnDefinition = "varchar(255) default ''")
+    private String email;
+
+    @Column(columnDefinition = "blob(255) default ''")
+    private byte[] passwordHashed;
+
+    @Column(columnDefinition = "blob(255) default ''")
+    private byte[] salt;
+
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+  private boolean isVerified
+
+}
