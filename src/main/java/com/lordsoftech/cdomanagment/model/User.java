628db65aad933e5cc86213a0c1a1ca9e73fadf3d
tylnesh <michal@kohutek.eu>
2022-06-22 13:50:08 +0200

    Scraped my own password managment methods and changed columns

--- src/main/java/com/lordsoftech/cdomanagment/model/User.java
+++ src/main/java/com/lordsoftech/cdomanagment/model/User.java
@@ -1,24 +1,21 @@
 package com.lordsoftech.cdomanagment.model;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 import java.sql.Timestamp;
+import java.util.List;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -28,8 +25,7 @@ import lombok.Data;
 public class User implements GenericEntity<User> {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
-  @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", updatable = false, nullable = false)
   private long id;
 
@@ -39,36 +35,24 @@ public class User implements GenericEntity<User> {
   @Column(columnDefinition = "varchar(255) default ''")
   private String password;
 
+  @CreationTimestamp
   @Column(name = "createdAt", columnDefinition = "timestamp default current_timestamp")
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Europe/Berlin")
   private Timestamp createdAt;
+
+  @UpdateTimestamp
   @Column(name = "updatedAt", columnDefinition = "timestamp default current_timestamp")
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Europe/Berlin")
   private Timestamp updatedAt;
 
-  private Timestamp tokenExpiration;
+  // private Timestamp tokenExpiration;
   private String token;
 
   private boolean isVerified;
 
-  @ManyToOne
-  @JoinColumn(name = "role_id")
-  private Role role;
-
-  // public byte[] hashPassword(String password, byte[] salt) throws
-  // InvalidKeySpecException, NoSuchAlgorithmException {
-  // SecretKeyFactory factory = null;
-  // factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-  // KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-  // return factory.generateSecret(spec).getEncoded();
-  // }
-
-  // public byte[] generateHashSalt() {
-  // SecureRandom random = new SecureRandom();
-  // byte[] salt = new byte[16];
-  // random.nextBytes(salt);
-  // return salt;
-  // }
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "role_id", joinColumns = {@JoinColumn(name="user_id")}, inverseJoinColumns = {@JoinColumn(name="role_id")}  )
+  private List<Role> role;
 
   public User() {
 
