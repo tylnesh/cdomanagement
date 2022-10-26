831daebac4f69638a7c680fc870ec9470a5147bd
tylnesh <michal@kohutek.eu>
2022-10-26 12:57:12 +0200

    Added randomized dealer and model generation

--- src/main/java/com/lordsoftech/cdomanagment/CdomanagmentApplication.java
+++ src/main/java/com/lordsoftech/cdomanagment/CdomanagmentApplication.java
@@ -1,12 +1,11 @@
 package com.lordsoftech.cdomanagment;
 
-import com.lordsoftech.cdomanagment.model.AppUser;
-import com.lordsoftech.cdomanagment.model.Dealer;
-import com.lordsoftech.cdomanagment.model.Printer;
-import com.lordsoftech.cdomanagment.model.Role;
+import com.lordsoftech.cdomanagment.model.*;
 import com.lordsoftech.cdomanagment.service.AppUserService;
 import com.lordsoftech.cdomanagment.service.DealerService;
+import com.lordsoftech.cdomanagment.service.ModelService;
 import com.lordsoftech.cdomanagment.service.PrinterService;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,7 +29,7 @@ public class CdomanagmentApplication {
 	}
 
 	@Bean
-	CommandLineRunner run(AppUserService userService, DealerService dealerService, PrinterService printerService) {
+	CommandLineRunner run(AppUserService userService, DealerService dealerService, PrinterService printerService, ModelService modelService) {
 		return args -> {
 			userService.saveRole(new Role("ROLE_USER"));
 			userService.saveRole(new Role("ROLE_DESIGNER"));
@@ -46,31 +45,12 @@ public class CdomanagmentApplication {
 			userService.addRoleToUser("test", "ROLE_DEALER");
 			userService.addRoleToUser("test", "ROLE_DESIGNER");
 
-			dealerService.saveDealer(new Dealer("Testing 1","t1"));
-			dealerService.saveDealer(new Dealer("Testing 2","t2"));
-			dealerService.saveDealer(new Dealer("Testing 3","t3"));
-			dealerService.saveDealer(new Dealer("Testing 4","t4"));
-			dealerService.saveDealer(new Dealer("Testing 5","t5"));
-			dealerService.saveDealer(new Dealer("Testing 6","t6"));
-			dealerService.saveDealer(new Dealer("Testing 7","t7"));
-			dealerService.saveDealer(new Dealer("Testing 8","t8"));
-			dealerService.saveDealer(new Dealer("Testing 9","t9"));
-			dealerService.saveDealer(new Dealer("Testing 10", "t10"));
-			dealerService.saveDealer(new Dealer("Testing 11", "t11"));
-			dealerService.saveDealer(new Dealer("Testing 12", "t12"));
-			dealerService.saveDealer(new Dealer("Testing 13", "t13"));
-			dealerService.saveDealer(new Dealer("Testing 14", "t14"));
-			dealerService.saveDealer(new Dealer("Testing 15", "t15"));
-			dealerService.saveDealer(new Dealer("Testing 16", "t16"));
-			dealerService.saveDealer(new Dealer("Testing 17", "t17"));
-			dealerService.saveDealer(new Dealer("Testing 18", "t18"));
-			dealerService.saveDealer(new Dealer("Testing 19", "t19"));
-			dealerService.saveDealer(new Dealer("Testing 20", "t20"));
-			dealerService.saveDealer(new Dealer("Testing 21", "t21"));
-			dealerService.saveDealer(new Dealer("Testing 22", "t22"));
-			dealerService.saveDealer(new Dealer("Testing 23", "t23"));
 
-
+			for (int i = 0; i< 25; i++) {
+				dealerService.saveDealer(new Dealer(RandomString.make(8), "d" + Integer.toString(i)));
+				short year = (short) (1990 + (short) (Math.random() * 2022));
+				modelService.saveModel(new Model(RandomString.make(4), RandomString.make(4), year, (short) (year + i)));
+			}
 			printerService.savePrinter(new Printer("NEW"));
 			printerService.savePrinter(new Printer("OLD"));
 		};
